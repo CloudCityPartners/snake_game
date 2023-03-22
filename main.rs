026b56f46f4940fa38d307cea7610ffba3a739ce
Rick <rjustiniano@cloudcitypartners.org>
2023-03-22 11:55:58 -0400

    Setting up Snake game Repo

---
+++ main.rs
@@ -0,0 +1,15 @@
+
+
+
+fn main() {
+    let mut message = "Hello World";
+
+    println!("{}", message);
+    message = "Hi There";
+    println!("Some text: {}", message);
+
+    let mut age = 10;
+    println!("{}", age);
+    age = 30;
+    println!("{}", age);
+}
